2554a63cd135ee4e1708b8ce5bb098932dfdfdcc
tzaitoun <talat.zaitoun@mail.utoronto.ca>
2018-11-30 19:09:27 -0500

    Added MongoDB to project

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
 
+require('./startup/db')();
+
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => console.log('Listening on port ${port}...'));
+const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
