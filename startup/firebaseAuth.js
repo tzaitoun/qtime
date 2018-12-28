0f9428bf57d2e13647b405ac17798899449cf1a6
tzaitoun <talat.zaitoun@mail.utoronto.ca>
2018-12-28 17:54:03 -0500

    Changed firebase auth startup file

--- startup/firebaseAuth.js
+++ startup/firebaseAuth.js
@@ -2,14 +2,7 @@ const admin = require('firebase-admin');
 const serviceAccount = require('../qtime-firebase.json');
 
 module.exports = function() {
-
-    if (process.env.NODE_ENV === 'production') {
-        const app = admin.initializeApp();
-    }
-
-    else {
-        const app = admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount)
-        });
-    }
+    const app = admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
 }
