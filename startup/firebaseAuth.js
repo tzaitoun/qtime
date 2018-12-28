6e7633cdc61554be91340857594e9c4635e13c22
tzaitoun <talat.zaitoun@mail.utoronto.ca>
2018-12-28 17:35:15 -0500

    Changed firebase initialization for production environment

--- startup/firebaseAuth.js
+++ startup/firebaseAuth.js
@@ -2,7 +2,14 @@ const admin = require('firebase-admin');
 const serviceAccount = require('../qtime-firebase.json');
 
 module.exports = function() {
-    const app = admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
-    });
+
+    if (process.env.NODE_ENV === 'production') {
+        const app = admin.initializeApp();
+    }
+
+    else {
+        const app = admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount)
+        });
+    }
 }
