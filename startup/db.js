fac1e9ed73bc4cd8d553089e82f362e67b4f7f37
tzaitoun <talat.zaitoun@mail.utoronto.ca>
2018-12-30 16:44:14 -0500

    Changed configuration of mongoose when in production

--- startup/db.js
+++ startup/db.js
@@ -5,7 +5,15 @@ const logger = require('../logger');
 module.exports = function() {
     const db = config.get('db');
     
-    mongoose.connect(db, { useNewUrlParser: true, auth: { authSource: 'admin' } })
-        .then(() => logger.info(`Connected to ${db}...`))
-        .catch((err) => logger.error(err));
+    if (process.env.NODE_ENV === 'production') {
+        mongoose.connect(db, { useNewUrlParser: true, auth: { authSource: 'admin' } })
+            .then(() => logger.info(`Connected to qtime database...`))
+            .catch((err) => logger.error(err));
+    }
+
+    else {
+        mongoose.connect(db, { useNewUrlParser: true })
+            .then(() => logger.info(`Connected to ${db}...`))
+            .catch((err) => logger.error(err));
+    }
 }
